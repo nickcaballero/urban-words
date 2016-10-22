63cc8c0e3c19e693915725443c6017cc6db786b2
Nick Caballero <nick.caballero@modernizingmedicine.com>
2016-10-22 13:42:55 -0400

    Adding the Alexa request body

---
+++ src/main/java/com/idklabs/alexa/amzn/AlexaRequest.java
@@ -0,0 +1,10 @@
+package com.idklabs.alexa.amzn;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author nick.caballero
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AlexaRequest {
+}
--- src/main/java/com/idklabs/alexa/urbanwords/UrbanWordsResource.java
+++ src/main/java/com/idklabs/alexa/urbanwords/UrbanWordsResource.java
@@ -1,7 +1,10 @@
 package com.idklabs.alexa.urbanwords;
 
+import com.idklabs.alexa.amzn.AlexaRequest;
 import com.idklabs.alexa.urbanwords.api.Definition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,8 @@ public class UrbanWordsResource {
     @Autowired
     private UrbanWordsService service;
 
-    @RequestMapping("/day")
-    public WordDefinition random() throws IOException {
+    @PostMapping("/day")
+    public WordDefinition getWordOfTheDay(@RequestBody AlexaRequest request) throws IOException {
         String wordOfTheDay = service.getWordOfTheDay();
         Optional<Definition> definition = service.getDefinition(wordOfTheDay)
                                                  .getDefinitions()
